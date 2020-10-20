3926e54e7bb7bac11d4d4432846da1105419e750
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-10-20 11:25:11 +0530

    final

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://SUBHRA:eutsI6OrrAtXNyHo@cluster0.ompsv.mongodb.net/devconnector?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
